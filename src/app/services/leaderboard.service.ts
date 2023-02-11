af5b8ccef4a4e188a83d7b25bddc514b32e693e1
downsd16 <downsd16@gmail.com>
2023-02-11 16:13:46 -0500

    configure leadeboard service for observables

--- src/app/services/leaderboard.service.ts
+++ src/app/services/leaderboard.service.ts
@@ -1,40 +1,18 @@
 import { Injectable } from '@angular/core';
-import { CosmosClient } from "@azure/cosmos";
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { ScoreItem } from './ScoreItem';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LeaderboardService {
 
-  private client: CosmosClient = new CosmosClient({ endpoint, key });
-  private db: any;
-  private container: any;
+  constructor(private http: HttpClient) {}
 
-  async ngOnInit(){
-    this.createClient()
+  ngOnInit(){}
+
+  getLeaderboard(): Observable<ScoreItem[]>{ 
+    return this.http.get<ScoreItem[]>("http://localhost:7071/api/CommandLeaderboardAPI")
   }
-
-  public async createClient(){
-    this.db = await this.client.databases.createIfNotExists({ id: DATABASE_NAME });
-    this.container = await this.db.containers.createIfNotExists({ id: CONTAINER_NAME });
-  }
-
-  public async getLeaderboard()
-  {
-    const querySpec = {
-      query: "SELECT * FROM leaderboard f WHERE  f.lastName = @lastName"
-    };
-  
-    const { resources: results } = await this.container.items.query(querySpec).fetchAll();
-  
-    if (results.length == 0) {
-      throw "No items found matching";
-    } else if (results.length > 1) {
-      throw "More than 1 item found matching";
-    }
-  
-    const person = results[0];
-    console.log("The '" + person.id + "' family has lastName '" + person.lastName + "'");
-    console.log("The '" + person.id + "' family has " + person.children.length + " children '");
-  } 
 }
